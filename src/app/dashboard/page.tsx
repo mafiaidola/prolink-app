cc94b688a0de057f0d4f7256deb46935a7872b3b
Mahmoud Mohamed <mafiaidola@gmail.com>
2025-09-15 20:17:15 +0000

    New Bio (_for element button_)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -20,7 +20,7 @@ export default async function DashboardPage() {
                 </div>
                 <Button>
                     <PlusCircle className="mr-2 h-4 w-4" />
-                    مش شغال
+                    New Bio
                 </Button>
             </div>
             <Card>
