4aae884a910ccf043c0da19c8c8e82fe062237ae
Mahmoud Mohamed <mafiaidola@gmail.com>
2025-09-15 21:58:58 +0000

    تصرف بإحترافيه وأصلح هذه المشكلة Runtime Error
    Error: Invalid src prop

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         hostname: 'cdn.simpleicons.org',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.svgrepo.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
